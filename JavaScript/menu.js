70edf1b19f446aaa31a5fea7507fefb812c1ceff
Akiozin <rm98582@fiap.com.br>
2023-08-30 13:38:01 -0300

    Preloader implementado

--- JavaScript/menu.js
+++ JavaScript/menu.js
@@ -1,1 +1,13 @@
 const toggleMenu = () => document.body.classList.toggle("open");
+
+// inicio preloader
+
+function closepreloader(){
+    document.getElementById("preloader").style.display = 'none';
+}
+window.addEventListener("load",function(){
+    setTimeout(closepreloader, 1000);
+    
+});
+
+// fim preloader
